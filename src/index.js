2094c9ca739a2253efae5ca824dd0af419825833
Adam K <krueng@gmail.com>
2021-09-29 19:58:46 -0500

    completed all tasks

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-
+import useDarkMode from "./hooks/useDarkMode";
 
 import "./styles.scss";
 
 const App = () => {
+  console.log('DM',useDarkMode);
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
